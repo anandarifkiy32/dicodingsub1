6a661164d39b0e41a404b5d6638b0e79b728e620
anandarifkiy32 <ananda.rifkiy32@gmail.com>
2019-04-12 11:12:46 +0700

    fix db connection v18

--- index.php
+++ index.php
@@ -2,4 +2,9 @@
     $connectionInfo = array("UID" => "rifkiystark@rifkiystarkdicoding", "pwd" => "Wakwaw123;", "Database" => "submis1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
     $serverName = "tcp:rifkiystarkdicoding.database.windows.net,1433";
     $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    $sql = "INSERT INTO Table_1 (UserId, Name, Email) VALUES (?, ?, ?)";
+    $params = array(1, "Kiki", "Ganteng");
+
+    sqlsrv_query( $conn , $sql, $params);
 ?>
