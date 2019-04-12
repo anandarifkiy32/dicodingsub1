0d2ae1c936b1949d3e7af81b16ecf237cdddc1af
anandarifkiy32 <ananda.rifkiy32@gmail.com>
2019-04-12 08:33:04 +0700

    fix db connection v1

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'rifkiystark',
 	'password' => 'Wakwaw123;',
 	'database' => 'submis1',
-	'dbdriver' => 'sqlsrv',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
