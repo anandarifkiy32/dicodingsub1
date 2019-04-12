9de0a1fc87277716bffb1c2160b74e5f0c3c9010
anandarifkiy32 <ananda.rifkiy32@gmail.com>
2019-04-12 11:18:53 +0700

    fix db connection v20

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
     $serverName = "tcp:rifkiystarkdicoding.database.windows.net,1433";
     $conn = sqlsrv_connect($serverName, $connectionInfo);
 
+    if( $conn ) {
+        echo "Connection established.<br />";
+   }else{
+        echo "Connection could not be established.<br />";
+        die( print_r( sqlsrv_errors(), true));
+   }
+   
     $sql = "INSERT INTO Users (UserId, Name, Email) VALUES (?, ?, ?)";
     $params = array(1, "Kiki", "Ganteng");
 
