770f62554f0aad0dc9bd25ce14fe5e542aebf61d
anandarifkiy32 <ananda.rifkiy32@gmail.com>
2019-04-12 10:54:54 +0700

    fix db connection v17

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php 
-try {
-    $conn = new PDO("sqlsrv:server = tcp:rifkiystarkdicoding.database.windows.net,1433; Database = submis1", "rifkiystark", "Wakwaw123;");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    $connectionInfo = array("UID" => "rifkiystark@rifkiystarkdicoding", "pwd" => "Wakwaw123;", "Database" => "submis1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:rifkiystarkdicoding.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
